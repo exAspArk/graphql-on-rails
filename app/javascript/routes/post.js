567e3ff1ee2102ea805c0f6ef16a6471b0621bba
exAspArk <exaspark@gmail.com>
2018-03-23 11:47:40 -0400

    Show post data by using GraphQL response

--- app/javascript/routes/post.js
+++ app/javascript/routes/post.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Query } from 'react-apollo'
 import Navbar from '../components/navbar'
 
-const Post = () => (
+import query from './post.gql'
+
+const Post = ({ post }) => (
   <div>
     <div className="container">
       <Navbar />
@@ -9,36 +12,14 @@ const Post = () => (
         <div className="row">
           <div className="col-md-8">
             <div>
-              <h2>Sample blog post</h2>
-              <p>January 1, 2014 by <a href="#">Mark</a></p>
+              <h2>{post.title}</h2>
 
-              <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
-              <hr />
-              <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-              <blockquote>
-                <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              </blockquote>
-              <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-              <h2>Heading</h2>
-              <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-              <h3>Sub-heading</h3>
-              <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-              <pre><code>Example code block</code></pre>
-              <p>Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-              <h3>Sub-heading</h3>
-              <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-              <ul>
-                <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-                <li>Donec id elit non mi porta gravida at eget metus.</li>
-                <li>Nulla vitae elit libero, a pharetra augue.</li>
-              </ul>
-              <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-              <ol>
-                <li>Vestibulum id ligula porta felis euismod semper.</li>
-                <li>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
-                <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
-              </ol>
-              <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.</p>
+              <p>
+                <span>{(new Date(post.createdAt)).toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' })} by </span>
+                <a href="#">{post.user.name}</a>
+              </p>
+
+              <div dangerouslySetInnerHTML={{ __html: post.content }} />
             </div>
           </div>
 
@@ -62,4 +43,17 @@ const Post = () => (
   </div>
 )
 
-export default Post
+export default (props) => {
+  const variables = { id: props.match.params.id };
+
+  return (
+    <Query query={query} variables={variables}>
+      {({ loading, error, data }) => {
+        if (loading) return <div>Loading...</div>
+        if (error) return <div>Error =/</div>
+
+        return <Post post={data.post} />
+      }}
+    </Query>
+  )
+}
