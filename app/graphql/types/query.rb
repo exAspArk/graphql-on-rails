40e895e14cd9866c931de0dfab61a5b7a8777387
exAspArk <exaspark@gmail.com>
2018-03-27 00:23:33 -0400

    Get Post by ID

--- app/graphql/types/query.rb
+++ app/graphql/types/query.rb
@@ -1,7 +1,14 @@
 class Types::Query < GraphQL::Schema::Object
   field :posts, [Types::Post], null: false
+  field :post, Types::Post, null: false do
+    argument :id, ID, required: true
+  end
 
   def posts
     ::Post.all
+  end
+
+  def post(attributes)
+    ::Post.find(attributes[:id])
   end
 end
