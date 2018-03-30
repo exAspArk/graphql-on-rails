e275f9d6d314db84d473793f5000d95d8f4fb5b4
exAspArk <exaspark@gmail.com>
2018-03-30 01:24:35 -0400

    Add User type

--- app/graphql/types/post.rb
+++ app/graphql/types/post.rb
@@ -4,4 +4,5 @@ class Types::Post < GraphQL::Schema::Object
   field :description, String, null: false
   field :content, String, null: false
   field :created_at, String, null: false
+  field :user, Types::User, null: false
 end
---
+++ app/graphql/types/user.rb
@@ -0,0 +1,5 @@
+class Types::User < GraphQL::Schema::Object
+  field :id, ID, null: false
+  field :name, String, null: false
+  field :created_at, String, null: false
+end
