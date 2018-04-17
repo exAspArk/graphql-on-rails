0effc55432bcd7f8d1dbd4eba7696ce98d87c3e3
exAspArk <exaspark@gmail.com>
2018-04-17 08:07:23 -0400

    Simplify Hello React

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,23 +4,11 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
+const App = () => (
+  <div>Hello React!</div>
 )
 
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
+  ReactDOM.render(<App />, document.body.appendChild(document.createElement('div')))
 })
